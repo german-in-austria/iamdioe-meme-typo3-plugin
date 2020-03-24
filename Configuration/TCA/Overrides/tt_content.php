1e0f4983deb1c77991580ba482da517924dc9c00
hcb79 <hcb@hcb.de>
2020-03-24 13:43:38 +0100

    flexform vorbereitet.

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,6 @@
+<?php
+
+
+// include the flexform for the plugin
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['hcbiamdioememe_fememegenerator'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('hcbiamdioememe_fememegenerator', 'FILE:EXT:hcb_iamdioe_meme/Configuration/FlexForms/flexform_memegenerator.xml');
