0ff1975b979eef18c13a71aca01677f627fb5f5f
hcb79 <hcb@hcb.de>
2020-03-25 10:09:04 +0100

    CSS für Meme Generator hinzugefügt.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -51,34 +51,4 @@ plugin.tx_hcbiamdioememe_fememelist {
 
 # these classes are only used in auto-generated templates
 plugin.tx_hcbiamdioememe._CSS_DEFAULT_STYLE (
-    textarea.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    input.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    .tx-hcb-iamdioe-meme table {
-        border-collapse:separate;
-        border-spacing:10px;
-    }
-
-    .tx-hcb-iamdioe-meme table th {
-        font-weight:bold;
-    }
-
-    .tx-hcb-iamdioe-meme table td {
-        vertical-align:top;
-    }
-
-    .typo3-messages .message-error {
-        color:red;
-    }
-
-    .typo3-messages .message-ok {
-        color:green;
-    }
 )
